b2be5269a49cfd3bbf06097eef3093ea09182f7c
Yanwei Du <yanwei.du@gatech.edu>
2025-10-01 17:07:05 -0400

    Add two more modes of capture triggering

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # insta360_ros_driver
 
-A ROS driver for the Insta360 cameras. This driver is tested on Ubuntu 20.04 with ROS Noetic. The driver has also been verified on the Insta360 X2 and X3 cameras.
+A ROS driver for the Insta360 cameras. This driver is tested on Ubuntu 20.04 with ROS Noetic.
 
 ## Installation
 To use this driver, you need to first have Insta360 SDK. Please apply for the SDK from the [Insta360 website](https://www.insta360.com/sdk/home). 
 
 ```
 cd ~/catkin_ws/src
-git clone https://github.com/ai4ce/insta360_ros_driver
+git clone git@github.com:ivaROS/insta360_ros_driver.git
 cd ..
 ```
 Then, the Insta360 libraries need to be installed as follows:
@@ -22,93 +22,30 @@ catkin build
 
 Before continuing, **make sure the camera is set to dual-lens mode**
 
-The Insta360 requires sudo privilege to be accessed via USB. To compensate for this, a udev configuration can be automatically created that will only request for sudo once. The camera can thus be setup initially via:
+The Insta360 requires **sudo privilege** to be accessed via USB. To compensate for this, a udev configuration can be automatically created that will only request for sudo once. The camera can thus be setup initially via:
 ```
-rosrun insta360_ros_driver setup.sh
+./setup.sh
 ```
 This creates a symlink  based on the vendor ID of Insta360 cameras. The symlink, in this case <code>/dev/insta</code> is used to grant permissions to the usb port used by the camera.
 ![setup](docs/setup.png)
 
 ## Usage
-This driver directly publishes the video feed in YUV format, since that is the camera's native setting. Alongside this, the driver also publishes the camera feed as standard BGR images to the <code>/front_camera_image/compressed</code> and <code>/back_camera_image/compressed</code> topics. Note that the compressed images have some amount of latency (~50 ms) compared to the raw output. 
-
-### Camera Bringup
-The camera can be brought up with the following launch file
-```
-roslaunch insta360_ros_driver bringup.launch
-```
-![bringup](docs/bringup_rqt.png)
-This publishes the raw yuv and compressed images as the following topics
-- /insta_image_yuv
-- /front_camera_image/compressed
-- /back_camera_image/compressed
-
-The launch file has the following optional arguments:
-- compress (default="true")
-
-Enables the <code>/live_processing</code> which publishes the compressed front and back images. When set to false, only the raw YUV image is published
-
-- undistort (default="false")
-
-When set to true, the <code>/live_processing</code> node will also use the specified intrinsic matrix, found in <code>config/config.yaml</code> to undistort the images in real-time. **Note that this process can reduce the frame rate and/or increase latency.** It is therefore recommended to keep it as 'false' and perform undistortion in post-processing, as will be shown in the next section.
-
-- debug (default="false")
-
-When set to true, additional diagnostic windows are created. These show the publishing frequency and image size. Note that **xterm** is required for this functionality, and can be installed using the following command.
-```
-sudo apt-get install xterm
+Launch the capturer:
+```bash
+roslaunch insta360_ros_driver online_capture.launch
 ```
 
-### Recording to a Bag File
-While the camera driver is active (via the bringup launch file above), data can be recorded to a bag file using
-```
-roslaunch insta360_ros_driver bag_record.launch
-```
-**The bag file save locations are specified within <code>config/config.yaml</code>**
-![config.yaml](docs/config.png)
+The capture script supports three trigger modes, checked in the following priority:
 
-The launch file contains one optional argument:
-- bag_type (default="compressed", options="\["raw", "compressed", "undistorted"\])
+1. **Service** – capture is triggered via a service call from other nodes.
 
-This specifies which directory to save the bag_files to, as specified in <code>config/config.yaml</code> When set to "raw", it will only record the <code>/insta_image_yuv</code> topic. Note that when specifying the <code>bag_type</code> to be "undistorted", <code>undistort:=true</code> must be set for <code>bringup.launch</code>
+2. **Pose checker** – monitors the `/odom` topic and triggers capture when translation exceeds a threshold (`trans_thresh`).
 
-The Raw YUV image appears as follows:
+3. **Timer** – captures images at a fixed interval set by a timer (`time_thresh`).
+Parameters
 
-![YUV](docs/yuv.png)
+Parameters:
+- `output_dir`: directory where the images are saved.
+- `time_thresh` (seconds): interval for timer-based capture.
 
-### Post-Processing: Image Compression and Splitting
-Given a bag file containing raw YUV image data, the images can be split to front and back and also compressed using the following launch file.
-```
-roslaunch insta360_ros_driver bag_compress.launch
-```
-This launch file reads all the bag files specified in the <code>raw_bag_folder</code> within the <code>config/config.yaml</code> file and outputs bag files containing split and compressed images into the <code>compressed_bag_folder</code> directory. The compressed images are within the <code>/front_camera_image/compressed</code> and <code>/back_camera_image/compressed</code> topics.
-
-After compression and splitting, the images appear as follows:
-![BGR](docs/bgr.png)
-
-### Post-Processing: Image Undistortion
-Given a bag file containing compressed front and back images, these images can be undistorted using the following launch file.
-```
-roslaunch insta360_ros_driver bag_undistort.launch
-```
-This launch file reads all the bag files specified in the <code>compressed_bag_folder</code> within the <code>config/config.yaml</code> file and outputs bag files undistorted images into the <code>undistorted_bag_folder</code> directory. The images are undistorted using the intrinsics specified inside the <code>config/config.yaml</code> file and are within the <code>/front_camera_image/compressed</code> and <code>/back_camera_image/compressed</code> topics.
-
-After undistortion, the images appear as follows:
-![undistorted](docs/undistort.png)
-
-### Bag Preview
-The compressed and undistorted bag files can be previewed using RViz via the following launch file.
-```
-roslaunch insta360_ros_driver bag_preview.launch
-```
-
-The launch file has the following argument:
-- bag_file (default="~/bag/undistorted/record.bag")
-
-This is the filepath of the bag file that will be previewed. Please make sure to change this accordingly. 
-
-When launched, an RViz window will be created showing both front and back images, as was previously demonstrated in earlier sections of the documentation
-
-## Star History
-
-[![Star History Chart](https://api.star-history.com/svg?repos=ai4ce/insta360_ros_driver&type=Date)](https://star-history.com/#ai4ce/insta360_ros_driver&Date)
+- `trans_thresh` (meters): translation threshold for pose-based capture.
---
+++ src/online_capturer.cpp
@@ -0,0 +1,392 @@
+/**
+ * @file online_capturer.cpp
+ * @author duyanwei (duyanwei0702@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-10-01
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include <camera/camera.h>
+#include <camera/device_discovery.h>
+#include <camera/photography_settings.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+#include <sensor_msgs/CompressedImage.h>
+#include <sensor_msgs/Imu.h>
+#include <std_srvs/Trigger.h>
+#include <unistd.h>
+
+#include <atomic>
+#include <boost/filesystem.hpp>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <thread>
+#include <vector>
+
+class TestStreamDelegate : public ins_camera::StreamDelegate {
+private:
+    ros::NodeHandle node_;
+    ros::Publisher  compressed_pub_;
+    ros::Publisher  imu_pub_;
+
+public:
+    TestStreamDelegate(const ros::NodeHandle& node) : node_(node) {
+        // Publisher for the compressed H.264 video stream
+        compressed_pub_ = node_.advertise<sensor_msgs::CompressedImage>(
+            "/insta/image/compressed", 10);
+
+        // Publisher for IMU data (remains the same)
+        imu_pub_ =
+            node_.advertise<sensor_msgs::Imu>("/insta/imu/data_raw", 100);
+        //        ROS_INFO("Publisher for compressed images and IMU created.");
+    }
+
+    virtual ~TestStreamDelegate() {}
+
+    void OnAudioData(const uint8_t* data, size_t size,
+                     int64_t timestamp) override {}
+
+    void OnVideoData(const uint8_t* data, size_t size, int64_t timestamp,
+                     uint8_t streamType, int stream_index) override {
+        return;
+        // We only care about the main video stream (index 0)
+        if (stream_index == 0 && size > 0) {
+            sensor_msgs::CompressedImage msg;
+
+            // Set the header
+            msg.header.stamp    = ros::Time::now();
+            msg.header.frame_id = "camera_frame";
+
+            // Set the format to H.264
+            // The subscriber will need to know this to select the correct
+            // decoder.
+            msg.format = "h264";
+
+            // Copy the compressed video data directly into the message
+            msg.data.assign(data, data + size);
+
+            // compressed_pub_.publish(msg);
+        }
+    }
+
+    void OnGyroData(const std::vector<ins_camera::GyroData>& data) override {
+        return;
+        for (const auto& gyro : data) {
+            sensor_msgs::Imu msg;
+            msg.header.stamp       = ros::Time::now();
+            msg.header.frame_id    = "imu_frame";
+            msg.angular_velocity.x = gyro.gx;
+            msg.angular_velocity.y = gyro.gy;
+            msg.angular_velocity.z = gyro.gz;
+
+            msg.linear_acceleration.x = gyro.ax * 9.80665;
+            msg.linear_acceleration.y = gyro.ay * 9.80665;
+            msg.linear_acceleration.z = gyro.az * 9.80665;
+
+            msg.orientation.x = 0.0;
+            msg.orientation.y = 0.0;
+            msg.orientation.z = 0.0;
+            msg.orientation.w = 1.0;  // Neutral orientation
+            msg.orientation_covariance[0] =
+                -1.0;  // No orientation data available
+
+            for (int i = 0; i < 9; i++) {
+                msg.angular_velocity_covariance[i]    = 0;
+                msg.linear_acceleration_covariance[i] = 0;
+            }
+            // imu_pub_.publish(msg);
+        }
+    }
+
+    void OnExposureData(const ins_camera::ExposureData& data) override {}
+};
+
+class CameraWrapper {
+public:
+    /**
+     * @brief Construct a new Camera Wrapper object
+     *
+     * @param node
+     */
+    CameraWrapper(const ros::NodeHandle& node) : node_(node), cam_(nullptr) {}
+
+    /**
+     * @brief Destroy the Camera Wrapper object
+     *
+     */
+    ~CameraWrapper() {
+        // Save Stamps
+        {
+            std::string   filename{output_dir_ + "/timestamps.txt"};
+            std::ofstream myfile(filename);
+            if (myfile.is_open()) {
+                for (const auto& s : stamps_) {
+                    myfile << s << "\n";
+                }
+                myfile.close();
+            }
+            ROS_INFO("Images with timestamps have been saved to %s .",
+                     output_dir_);
+        }
+        if (cam_) {
+            cam_->Close();
+            ROS_INFO("Camera has been closed.");
+        }
+    }
+
+    int InitCam() {
+        ROS_INFO("Start to open Camera ...");
+        ins_camera::DeviceDiscovery discovery;
+        auto                        list = discovery.GetAvailableDevices();
+        if (list.empty()) {
+            ROS_ERROR("No available camera devices found.");
+            return -1;
+        }
+
+        cam_ = std::make_shared<ins_camera::Camera>(list[0].info);
+        if (!cam_->Open()) {
+            ROS_ERROR("Failed to open camera.");
+            return -1;
+        }
+        ROS_INFO("Camera opened successfully.");
+        discovery.FreeDeviceDescriptors(list);
+
+        std::shared_ptr<ins_camera::StreamDelegate> delegate =
+            std::make_shared<TestStreamDelegate>(node_);
+        cam_->SetStreamDelegate(delegate);
+
+        auto start = time(NULL);
+        cam_->SyncLocalTimeToCamera(start);
+        ins_camera::LiveStreamParam param;
+        param.video_resolution = ins_camera::VideoResolution::
+            RES_3840_1920P30;  // Change this line to edit the resolution
+        // Possible resolutions (results may vary per model) are:
+        // RES_3840_1920P30
+        // RES_2560_1280P30
+        // RES_1152_1152P30 (this will give 2304 x 1152 at 30 FPS)
+        // RES_1920_960P30
+        param.lrv_video_resulution =
+            ins_camera::VideoResolution::RES_1440_720P30;
+        param.video_bitrate = 1024 * 1024 / 2;
+        param.enable_audio  = false;
+        param.using_lrv     = false;
+
+        if (!cam_->StartLiveStreaming(param)) {
+            ROS_ERROR("Failed to start live streaming.");
+            return -1;
+        }
+
+        // Enable online stitching.
+        bool enable_stitching = true;
+        if (cam_->EnableInCameraStitching(enable_stitching)) {
+            ROS_INFO("In-camera stitching function activated successfully!");
+        } else {
+            ROS_WARN("In-camera stitching is NOT activated!");
+        }
+
+        ROS_INFO("Live streaming started.");
+        return 0;
+    }
+
+    void InitRos() {
+        enable_service_      = node_.param<bool>("enable_service", false);
+        enable_timer_        = node_.param<bool>("enable_timer", false);
+        enable_pose_checker_ = node_.param<bool>("enable_pose_checker", false);
+
+        time_thresh_  = node_.param<float>("time_thresh", 5);     // seconds
+        trans_thresh_ = node_.param<float>("trans_thresh", 2.0);  // meters
+
+        if (enable_service_) {
+            enable_timer_        = false;
+            enable_pose_checker_ = false;
+            service_             = node_.advertiseService(
+                "/capture", &CameraWrapper::captureRequest, this);
+            ROS_INFO("Service mode enabled!!!");
+        } else if (enable_pose_checker_) {
+            odom_sub_ =
+                node_.subscribe("/odom", 10, &CameraWrapper::odomCb, this);
+            enable_timer_ = false;
+            ROS_INFO("PoseChecker mode enabled!!!");
+        }
+
+        if (enable_timer_) {
+            time_thresh_ = time_thresh_ < 0.0 ? 5.0 : time_thresh_;
+            timer_       = node_.createTimer(ros::Duration(time_thresh_),
+                                             &CameraWrapper::timerCallback, this);
+            ROS_INFO("Timer mode enabled!!!");
+        }
+
+        // ROS Parameters.
+        output_dir_ =
+            node_.param<std::string>("output_dir", "/tmp/insta/");  // meters
+        if (!endsWithSlash(output_dir_)) {
+            output_dir_.append("/");
+        }
+
+        output_dir_ += getCurrentTimeAsDirectoryString();
+        boost::filesystem::create_directories(output_dir_);
+    }
+
+    /**
+     * @brief
+     *
+     * @param event
+     */
+    void timerCallback(const ros::TimerEvent& event) { capture(); }
+
+    void odomCb(const nav_msgs::OdometryConstPtr& msg) {
+        float cur_tx = msg->pose.pose.position.x;
+        float cur_ty = msg->pose.pose.position.y;
+
+        if (last_pose_.isValid()) {
+            float dist2 = (cur_tx - last_pose_.tx) * (cur_tx - last_pose_.tx) +
+                          (cur_ty - last_pose_.ty) * (cur_ty - last_pose_.ty);
+            if (dist2 > trans_thresh_ * trans_thresh_) {
+                capture();
+            }
+        }
+
+        // thread safe.
+        last_pose_.timestamp = msg->header.stamp.toSec();
+        last_pose_.tx        = msg->pose.pose.position.x;
+        last_pose_.ty        = msg->pose.pose.position.y;
+    }
+
+    bool captureRequest(std_srvs::Trigger::Request&  req,
+                        std_srvs::Trigger::Response& res) {
+        ROS_INFO("Capturing Service is Triggered!");
+        res.success = capture();
+        res.message = res.success ? "Image Captured" : "Fail to capture!";
+        return true;
+    }
+
+    /**
+     * @brief
+     *
+     */
+    bool capture() {
+        if (!cam_) {
+            ROS_ERROR("Camera is not initialized!");
+            return false;
+        }
+        bool ret =
+            cam_->SetPhotoSubMode(ins_camera::SubPhotoMode::PHOTO_SINGLE);
+        if (!ret) {
+            ROS_ERROR("change sub mode failed!");
+            return false;
+        }
+
+        ROS_INFO("Taking a picture ...");
+        double     timestamp = ros::Time::now().toSec();
+        const auto url       = cam_->TakePhoto();
+        if (!url.IsSingleOrigin() || url.Empty()) {
+            ROS_ERROR("failed to take picture");
+            return false;
+        }
+
+        const std::string download_url = url.GetSingleOrigin();
+        const std::string file_name    = this->getFileName(download_url);
+
+        std::string save_path = output_dir_ + file_name;
+        ret = cam_->DownloadCameraFile(download_url, save_path);
+        if (ret) {
+            ROS_INFO_STREAM("Download " << download_url << " succeed!!!");
+            ROS_INFO_STREAM("Image saved to: " << save_path);
+            stamps_.emplace_back(timestamp, file_name);
+        } else {
+            ROS_ERROR_STREAM("Download " << download_url << " failed!!!");
+        }
+        return true;
+    }
+
+    void run() { ros::spin(); }
+
+private:
+    std::string getCurrentTimeAsDirectoryString() {
+        ros::Time   now = ros::Time::now();
+        std::time_t t   = (time_t)now.sec;  // Convert ROS time to std::time_t
+
+        std::stringstream ss;
+        // Format as YYYY-MM-DD-HH-MM-SS
+        ss << std::put_time(std::localtime(&t), "%Y-%m-%d-%H-%M-%S");
+        return ss.str();
+    }
+
+    std::string getFileName(const std::string& path) {
+        std::smatch sm;
+        std::string dir, name;
+        std::regex_match(path, sm, std::regex("(.+?)([^\\/\\\\]+$)"));
+        if (sm.size() <= 2) {
+            return path;
+        }
+        return sm[2].str();
+    }
+
+    bool endsWithSlash(const std::string& path) {
+        if (path.empty()) {
+            return false;
+        }
+        return path.back() == '/' || path.back() == '\\';
+    }
+
+    struct StampedImage {
+        double      timestamp;
+        std::string filename;
+
+        StampedImage() = default;
+        StampedImage(double _timestamp, const std::string& _filename)
+            : timestamp(_timestamp), filename(_filename) {}
+
+        friend std::ostream& operator<<(std::ostream&       os,
+                                        const StampedImage& s) {
+            os << std::fixed << std::setprecision(6) << s.timestamp << " "
+               << s.filename;
+            return os;
+        }
+    };
+
+    struct StampedPose {
+        double timestamp = -1.0;
+        double tx = 0.0, ty = 0.0, theta = 0.0;
+        bool   isValid() const { return timestamp >= 0.0; }
+    };
+
+    ros::NodeHandle                     node_;
+    std::shared_ptr<ins_camera::Camera> cam_;
+    std::vector<StampedImage>           stamps_;
+    ros::ServiceServer                  service_;
+    ros::Subscriber                     odom_sub_;
+    ros::Timer                          timer_;
+
+    std::string output_dir_{"/tmp/insta/"};
+    bool        enable_service_{false};
+    bool        enable_pose_checker_{false};
+    bool        enable_timer_{false};
+    std::mutex  pose_mutex_;
+    StampedPose last_pose_;
+    float       time_thresh_{-1.0};
+    float       trans_thresh_{-1.0};
+    float       rot_thresh_{-1.0};
+};
+
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "insta_capturer");
+    ros::NodeHandle nh("~");
+    CameraWrapper   camera(nh);
+    if (camera.InitCam() != 0) {
+        ros::shutdown();
+        return -1;
+    }
+    camera.InitRos();
+    camera.run();
+    ros::spin();
+    ros::shutdown();
+    ROS_INFO("Done");
+    return 0;
+}
